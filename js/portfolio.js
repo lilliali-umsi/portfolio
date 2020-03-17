a4078706c6737a7c1092700da67204cc2d30796a
lilliali-umsi <55165046+lilliali-umsi@users.noreply.github.com>
2020-03-17 18:10:27 -0400

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,68 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("nav_links");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
 
-var url = "http://example.com/products.html".split("/"); //replace string with location.href
-var navLinks = document.getElementsByClass("nav_links")[0].getElementsByTagName("a");
-//naturally you could use something other than the <nav> element
-var i=0;
-var currentPage = url[url.length - 1];
-for(i;i<navLinks.length;i++){
-  var lb = navLinks[i].href.split("/");
-  if(lb[lb.length-1] == currentPage) {
-   navLinks[i].className = "current";
+var myIndex = 0;
 
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
-  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+
+// var slideIndex = 1;
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides2");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
+// // https://www.w3schools.com/w3css/w3css_slideshow.asp
+// var motionQuery = matchMedia('(prefers-reduced-motion)');
+// function handleReduceMotionChanged(){
+// 	if (motionQuery.matches){
+// 		 carousel('disabled');
+
+
+// }else{
+// 	showDivs('disabled');
+
+// }
+// }
+// https://justmarkup.com/articles/2019-02-19-adapting-to-user-preferences/
+
+
+function mail(){
+	window.location.href="mailto:lillianliauthor@gmai.com";
+}
